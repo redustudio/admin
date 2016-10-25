839345c1e271b2ff63c9d985dde2ec48ac3d125e
Ramadani <email.ramadani@gmail.com>
2016-10-25 21:29:49 +0700

    fix psr2

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,8 @@ $router->post('/login', ['as' => 'reduvel-admin:login', 'uses' => 'Auth\LoginCon
 $router->get('/logout', ['as' => 'reduvel-admin:logout', 'uses' => 'Auth\LoginController@logout']);
 
 $router->group(['middleware' => ['auth']], function ($router) {
-    $router->get(config('reduvel.admin.route.web.index'), ['as' => 'reduvel-admin:index', 'uses' => 'Controller@index']);
+    $router->get(config('reduvel.admin.route.web.index'), [
+        'as' => 'reduvel-admin:index',
+        'uses' => 'Controller@index'
+    ]);
 });
